6b72164e969a39c5eddd3c06af4e4dffc2005216
Sue Park <suepandrice06@gmail.com>
2019-09-14 12:50:38 -0500

    completed exercises

--- server.js
+++ server.js
@@ -19,34 +19,54 @@ var tweets = require('./abstractions/tweets.js');
 app.use(express.static(__dirname + '/public'));
 
 // GET /api/albums
-
+app.get('/api/albums', function (request, response, nextFn) {
+    response.send(albums)
+})
 
 // GET /api/circles
-
+app.get('/api/circles', function (request, response, nextFn) {
+    response.send(circles)
+})
 
 // GET /api/movies
-
+app.get('/api/movies', function (request, response, nextFn) {
+    response.send(movies)
+})
 
 // GET /api/nametags
-
+app.get('/api/nametags', function (request, response, nextFn) {
+    response.send(nameTags)
+})
 
 // GET /api/poker
-
+app.get('/api/poker', function (request, response, nextFn) {
+    response.send(poker)
+})
 
 // GET /api/rectangle
-
+app.get('/api/rectangle', function (request, response, nextFn) {
+    response.send(rectangle)
+})
 
 // GET /api/restaurants
-
+app.get('/api/restaurants', function (request, response, nextFn) {
+    response.send(restaurants)
+})
 
 // GET /api/students
-
+app.get('/api/students', function (request, response, nextFn) {
+    response.send(students)
+})
 
 // GET /api/surveys
-
+app.get('/api/surveys', function (request, response, nextFn) {
+    response.send(surveys)
+})
 
 // GET /api/tweets
-
+app.get('/api/tweets', function (request, response, nextFn) {
+    response.send(tweets)
+})
 
 app.listen(3000, function(){
     console.log('rendering-api is now listening on port 3000...');
